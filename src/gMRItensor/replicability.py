ec592d4438531343264805e22615f30dbe5a2dcf
erasdna <solheim@simula.no>
2026-07-27 15:29:17 +0200

    fix bug in half-half split

--- src/gMRItensor/replicability.py
+++ src/gMRItensor/replicability.py
@@ -17,17 +17,14 @@ def half_half_split_replicability(
         half_half_factors = []
         for iids in [shuffled_indices[:half_size], shuffled_indices[half_size:]]:
             use_tensor = tensor[iids]
-            print(use_tensor.shape)
-            print(use_tensor.device)
             weights, factors, best_error = run_CP_decomposition_repeated(
                 use_tensor, rank, device=tensor.device, **kwargs
             )
-            half_half_factors.append((weights, factors))
+            half_half_factors.append((weights, factors[1:]))
 
         fms = factor_match_score(
             half_half_factors[0],
             half_half_factors[1],
-            skip_mode=0,
             consider_weights=False,
         )
 
