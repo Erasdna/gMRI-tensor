e4ed4b36eaba0a9994068f8214234243a153f2c2
erasdna <solheim@simula.no>
2026-08-07 09:45:32 +0200

    add plotting

--- src/gMRItensor/plotting.py
+++ src/gMRItensor/plotting.py
@@ -155,8 +155,6 @@ def plot_subject_mode(
     fig, axs = plt.subplots(
         subject_mode.shape[1],
         1 + len(plotting_variables),
-        sharey=True,
-        sharex=True,
         figsize=figsize,
     )
     for i, ax in enumerate(axs):
