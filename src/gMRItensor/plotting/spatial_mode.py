1f95dcca6a007263b8c9ab422a6c76e3b8746fef
erasdna <solheim@simula.no>
2026-08-10 13:40:10 +0200

    spacing

--- src/gMRItensor/plotting/spatial_mode.py
+++ src/gMRItensor/plotting/spatial_mode.py
@@ -55,8 +55,10 @@ def make_colorbar(fig, ax, cax, cax_divider, label, shrink):
         shrink=shrink,
         format=formatter,
     )
+    # Position exponent on the right side below the top edge
     cbar.ax.yaxis.offsetText.set_visible(True)
-    cbar.ax.yaxis.offsetText.set_horizontalalignment("right")
+    cbar.ax.yaxis.set_offset_position("right")
+    cbar.ax.yaxis.offsetText.set_horizontalalignment("left")
     cbar.set_label(label=label)
 
 
