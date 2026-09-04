044d69bb8cf394423002877dc3004e083a12ff75
erasdna <solheim@simula.no>
2026-09-04 15:43:46 +0200

    minor bug

--- src/gMRItensor/plotting/utils.py
+++ src/gMRItensor/plotting/utils.py
@@ -204,7 +204,7 @@ def merge_segmentations(
 
     if label_overrides:
         for label_id, target_name in label_overrides.items():
-            if target_name not in segmentations:
+            if target_name not in segmentations.keys():
                 raise ValueError(
                     f"Override target {target_name!r} is not in segmentations",
                 )
